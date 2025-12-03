6a523a2a2e584f4f333ca3ba59612aead437ddf3
Patson Tembo <tembopatson5@gmail.com>
2025-12-03 08:29:53 +0200

    Create index.php to show PHP configuration
    Add PHP info function to display configuration

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// index.php
+
+// This function outputs the server's entire PHP configuration.
+// If you see a detailed table, PHP is working.
+// ALWAYS remove this from production.
+phpinfo();
+?>
